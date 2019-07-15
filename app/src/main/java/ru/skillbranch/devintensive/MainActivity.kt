0e72f35f4d36d20b5c924b46213e853a465f1a9e
grigorii <leogrisha@mail.ru>
2019-07-15 20:45:29 +0300

    hometask_3 complete

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,12 +1,149 @@
 package ru.skillbranch.devintensive
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.text.isDigitsOnly
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.extensions.hideKeyboard
+import ru.skillbranch.devintensive.models.Bender
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener, TextView.OnEditorActionListener {
+
+    lateinit var benderImage: ImageView
+    lateinit var textTxt: TextView
+    lateinit var messageEt: EditText
+    lateinit var sendBtn: ImageView
+    lateinit var benderObj: Bender
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val status = savedInstanceState?.getString("STATUS") ?: Bender.Status.NORMAL.name
+        val question = savedInstanceState?.getString("QUESTION") ?: Bender.Question.NAME.name
+
+        benderImage = iv_bender
+        textTxt = tv_text
+        messageEt = et_message
+        sendBtn = iv_send
+
+        benderObj = Bender(Bender.Status.valueOf(status), Bender.Question.valueOf(question))
+
+        val (r, g, b) = benderObj.status.color
+        benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
+
+        sendBtn.setOnClickListener(this)
+        messageEt.setOnEditorActionListener(this)
+
+        textTxt.text = benderObj.askQuestion()
+
+    }
+
+    override fun onClick(v: View?) {
+
+        if (v?.id == R.id.iv_send){
+
+            hideKeyboard()
+            et_message.setText("")
+
+            val answer = et_message.text.toString()
+
+            when(benderObj.question) {
+                Bender.Question.NAME -> if(answer[0].isLowerCase()) {
+                    val textForTv = "Имя должно начинаться с заглавной буквы\n${benderObj.askQuestion()}"
+                    textTxt.text = textForTv
+                    return
+                    }
+                Bender.Question.PROFESSION -> if(answer[0].isUpperCase()) {
+                    val textForTv = "Профессия должна начинаться со строчной буквы\n${benderObj.askQuestion()}"
+                    textTxt.text = textForTv
+                    return
+                }
+                Bender.Question.MATERIAL -> if(answer.contains(Regex("[1-9]"))) {
+                    val textForTv = "Материал не должен содержать цифр\n${benderObj.askQuestion()}"
+                    textTxt.text = textForTv
+                    return
+                }
+                Bender.Question.BDAY -> if(!answer.isDigitsOnly()) {
+                    val textForTv = "Год моего рождения должен содержать только цифры\n${benderObj.askQuestion()}"
+                    textTxt.text = textForTv
+                    return
+                }
+                Bender.Question.SERIAL -> if(!(answer.isDigitsOnly() && answer.length == 7)) {
+                    val textForTv = "Серийный номер содержит только цифры и их 7\n${benderObj.askQuestion()}"
+                    textTxt.text = textForTv
+                    return
+                }
+                Bender.Question.IDLE -> return
+            }
+
+            val (phrase, color) = benderObj.listenAnswer(answer.toLowerCase())
+            val (r, g, b) = color
+            benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
+            textTxt.text = phrase
+
+        }
+    }
+
+    override fun onEditorAction(p0: TextView?, p1: Int, p2: KeyEvent?): Boolean {
+
+        if (p1 == EditorInfo.IME_ACTION_DONE) {
+
+            hideKeyboard()
+            et_message.setText("")
+
+            val answer = et_message.text.toString()
+
+            when(benderObj.question) {
+                Bender.Question.NAME -> if(answer[0].isLowerCase()) {
+                    val textForTv = "Имя должно начинаться с заглавной буквы\n${benderObj.askQuestion()}"
+                    textTxt.text = textForTv
+                    return true
+                }
+                Bender.Question.PROFESSION -> if(answer[0].isUpperCase()) {
+                    val textForTv = "Профессия должна начинаться со строчной буквы\n${benderObj.askQuestion()}"
+                    textTxt.text = textForTv
+                    return true
+                }
+                Bender.Question.MATERIAL -> if(answer.contains(Regex("[1-9]"))) {
+                    val textForTv = "Материал не должен содержать цифр\n${benderObj.askQuestion()}"
+                    textTxt.text = textForTv
+                    return true
+                }
+                Bender.Question.BDAY -> if(!answer.isDigitsOnly()) {
+                    val textForTv = "Год моего рождения должен содержать только цифры\n${benderObj.askQuestion()}"
+                    textTxt.text = textForTv
+                    return true
+                }
+                Bender.Question.SERIAL -> if(!(answer.isDigitsOnly() && answer.length == 7)) {
+                    val textForTv = "Серийный номер содержит только цифры и их 7\n${benderObj.askQuestion()}"
+                    textTxt.text = textForTv
+                    return true
+                }
+                Bender.Question.IDLE -> return true
+            }
+
+            val (phrase, color) = benderObj.listenAnswer(et_message.text.toString().toLowerCase())
+            val (r, g, b) = color
+            benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
+            textTxt.text = phrase
+
+            return true
+        }
+        return false
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("QUESTION", benderObj.question.name)
+        outState.putString("STATUS", benderObj.status.name)
     }
 }
---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -0,0 +1,69 @@
+package ru.skillbranch.devintensive.models
+
+class Bender(var status: Status = Status.NORMAL, var question: Question = Question.NAME) {
+
+
+    enum class Status(val color: Triple<Int, Int, Int>) {
+        NORMAL(Triple(255, 255, 255)) ,
+        WARNING(Triple(255, 120, 0)),
+        DANGER(Triple(255, 60, 60)),
+        CRITICAL(Triple(255, 0, 0));
+
+        fun nextStatus(): Status =
+            if (this.ordinal < values().lastIndex) {
+                values()[this.ordinal + 1]
+            } else {
+                values()[0]
+            }
+    }
+
+    enum class Question(val question: String, val answers: List<String>) {
+
+        NAME("Как меня зовут?", listOf("бендер", "bender", "1")) {
+            override fun nextQuestion(): Question = PROFESSION
+        },
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender", "1")) {
+            override fun nextQuestion(): Question = MATERIAL
+        },
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood", "1")) {
+            override fun nextQuestion(): Question = BDAY
+        },
+        BDAY("Когда меня создали?", listOf("2993", "1")) {
+            override fun nextQuestion(): Question = SERIAL
+        },
+        SERIAL("Мой серийный номер?", listOf("2716057", "1")) {
+            override fun nextQuestion(): Question = IDLE
+        },
+        IDLE("На этом все, вопросов больше нет", listOf()) {
+            override fun nextQuestion(): Question = IDLE
+        };
+
+        abstract fun nextQuestion(): Question
+    }
+
+    fun askQuestion(): String = when(question) {
+        Question.NAME -> Question.NAME.question
+        Question.PROFESSION -> Question.PROFESSION.question
+        Question.MATERIAL -> Question.MATERIAL.question
+        Question.BDAY -> Question.BDAY.question
+        Question.SERIAL -> Question.SERIAL.question
+        Question.IDLE -> Question.IDLE.question
+    }
+
+    fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
+        return if (question.answers.contains(answer)) {
+            question = question.nextQuestion()
+            "${if(question == Question.IDLE)"Отлично - ты справился!" else "Отлично - это правильный ответ!"}\n" +
+                    question.question to status.color
+        } else {
+            if (status == Status.CRITICAL) {
+                question = Question.NAME
+                status = Status.NORMAL
+                "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
+            } else {
+                status = status.nextStatus()
+                "Это неправильный ответ!\n${question.question}" to status.color
+            }
+        }
+    }
+}
